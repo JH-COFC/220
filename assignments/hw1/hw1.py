04c30d336b31db25bbfacadab79f616e3e2054f6
JH-CofC <herderjs@g.cofc.edu>
2022-01-19 13:12:17 -0500

    homework 1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,13 +1,12 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Jordan Herder>
+<hw1>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <Series of functions to calculate area, volume, successful shot percentage, coffee shipment cost, and kilometer to mile conversion.>
 
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
