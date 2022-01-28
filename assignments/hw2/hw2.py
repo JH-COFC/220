f5dc077db44fa7cdd71a5c8536a57bbfc5af3b23
JH-CofC <herderjs@g.cofc.edu>
2022-01-28 13:55:58 -0500

    homework 2

--- assignments/hw2/hw2.py
+++ assignments/hw2/hw2.py
@@ -1,35 +1,59 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Jordan Herder
+hw2.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Contains functions which may be used to calculate the sum of threes, produce the multiplication table,
+calculate triangle area, determine the sum of squares, and manually calculate a power result.
 
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 import math
 
 
 def sum_of_threes():
-    pass
+    answer = 0
+    upper_bound = eval(input("What is the upper bound?")) + 1
+    for i in range(0, upper_bound, 3):
+        answer = answer + i
+    print("Sum of threes is",answer)
 
 
 def multiplication_table():
-    pass
+    for i in range(1,11):
+        for j in range(1,11):
+            number = j*i
+            print(number,end="\t")
+        print("\n")
 
 
 def triangle_area():
-    pass
+    sidea = eval(input("Enter side a length:"))
+    sideb = eval(input("Enter side b length:"))
+    sidec = eval(input("Enter side c length:"))
+    value_s = (sidea+sideb+sidec)/2
+    answer = math.sqrt(value_s*(value_s-sidea)*(value_s-sideb)*(value_s-sidec))
+    print("area is",answer)
 
 
 def sum_squares():
-    pass
+    answer = 0
+    lower_range = eval(input("Enter lower range:"))
+    upper_range = eval(input("Enter upper range:")) +1
+    for i in range(lower_range,upper_range):
+        answer = answer + i*i
+    print(answer)
+
 
 
 def power():
-    pass
+    base = eval(input("Enter base"))
+    exponent = eval(input("Enter exponent"))
+    answer = base
+    for i in range(exponent-1):
+        answer = answer*base
+    print(base,"^",exponent,"=",answer)
 
 
 if __name__ == '__main__':
