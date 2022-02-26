786c12b0898b28e70e4079b32031566705f564b9
JH-CofC <herderjs@g.cofc.edu>
2022-02-25 23:03:04 -0500

    homework 6

--- assignments/hw6/hw6.py
+++ assignments/hw6/hw6.py
@@ -1,42 +1,71 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Jordan Herder>
+<hw6>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <Program displays the cash value of a number, encodes a message,
+calculates the surface area and volume of a sphere,
+calculates a set of summed numbers and their cubes,
+and encodes a message differently. >
 
 Certification of Authenticity:
 <include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+I certify that this assignment is entirely my own work.>
 """
+
+from math import pi
 
 
 def cash_converter():
-    pass
+    user_input = eval(input("Enter an integer:"))
+    print("That is ${:.2f}".format(user_input))
 
 
 def encode():
-    pass
+    message = input("Enter a message:")
+    key = eval(input("Enter a key:"))
+    coded_message = []
+    output = ""
+    for i in message:
+        coded_message = coded_message+[(ord(i)+key)]
+    for j in coded_message:
+        output = output+chr(j)
+    print(output)
 
 
 def sphere_area(radius):
-    pass
+    area = (4*pi*radius**2)
+    return area
 
 
 def sphere_volume(radius):
-    pass
+    volume = (4/3)*pi*radius**3
+    return volume
 
 
 def sum_n(number):
-    pass
+    output = 0
+    for i in range(1,number+1):
+        output = output+i
+    return output
 
 
 def sum_n_cubes(number):
-    pass
+    output = 0
+    for i in range(1,number+1):
+        output = output+i**3
+    return output
 
 
 def encode_better():
-    pass
+    message = input("Enter a message:")
+    key = input("Enter a key:")
+    coded_message = []
+    output = ""
+    for i in range(len(message)):
+        coded_message = coded_message + [((ord(message[i])-65)+(ord(key[i % 3])-65)) % 58]
+    for j in coded_message:
+        output = output+chr(j+65)
+    print(output)
 
 
 if __name__ == '__main__':
